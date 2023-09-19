94ad0e22ec3c4c01ac4d98b9b10f112144d3c6f6
Farah Majdalani <farahmmajdalani@gmail.com>
2023-09-19 17:01:33 +0200

    account id

--- src/main/webapp/app/core/util/cart.service.ts
+++ src/main/webapp/app/core/util/cart.service.ts
@@ -3,6 +3,9 @@ import { Subject } from 'rxjs';
 import { IProduit } from 'app/entities/produit/produit.model';
 import { LigneCommandeService } from 'app/entities/ligne-commande/service/ligne-commande.service';
 import { ILigneCommande } from 'app/entities/ligne-commande/ligne-commande.model';
+import { AccountService } from '../auth/account.service';
+import { Account } from '../auth/account.model';
+import dayjs from 'dayjs/esm';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +13,12 @@ import { ILigneCommande } from 'app/entities/ligne-commande/ligne-commande.model
 export class CartService {
   counter: number = 0;
   cart: ILigneCommande[] = [];
+  account: Account | null;
   // ligneCommande: NewLigneCommande;
 
   counterChange: Subject<number> = new Subject<number>();
 
-  // constructor(private ligneCommandeService: LigneCommandeService) {}
+  constructor(private ligneCommandeService: LigneCommandeService, private accountService: AccountService) {}
 
   // addToCart(course: IProduit, num: number): void {
   //   this.ligneCommande = {
@@ -34,41 +38,62 @@ export class CartService {
   // }
 
   addToCart(course: IProduit, num: number): void {
-    this.cart.push({
-      id: -1,
-      produit: course,
-      quantite: num,
-    });
-    this.counterChange.next((this.counter += num));
+    // this.cart.push({
+    //   id: -1,
+    //   produit: course,
+    //   quantite: num,
+    // });
+    this.accountService
+      .identity()
+      .pipe()
+      .subscribe(account => (this.account = account));
+
+    this.ligneCommandeService
+      .create({
+        id: null,
+        quantite: num,
+        montant: course.tarifUnit! * num,
+        validated: 0,
+        nomParticipant: this.account?.firstName + ' ' + this.account?.lastName,
+        createdAt: dayjs(),
+        updateAt: dayjs(),
+        produit: course,
+        commande: {
+          id: this.account?.id ? this.account.id : -1,
+        },
+      })
+      .subscribe({
+        next: value => this.counterChange.next((this.counter += num)),
+        error: error => console.log(error),
+      });
   }
 
   changeToCart(course: IProduit, num: number): void {
-    this.cart.forEach((commande) => {
-      if(course == commande.produit){
+    this.cart.forEach(commande => {
+      if (course == commande.produit) {
         this.counterChange.next((this.counter = this.counter - commande.quantite! + num));
-        commande.quantite = num
+        commande.quantite = num;
       }
-    })
+    });
   }
 
   deleteCartProducts(commandeChoosen: ILigneCommande): void {
-
-    this.cart.forEach((commande, index) => {
-      if(commandeChoosen == commande){
-        this.cart.splice(index,1)
-        this.counterChange.next((this.counter -= commande.quantite!));
+    this.cart.forEach((item, index) => {
+      if (item.id === commandeChoosen.id) {
+        this.cart.splice(index, 1);
+        this.counterChange.next((this.counter -= commandeChoosen.quantite!));
       }
-    })
+    });
   }
 
   getCartProducts(): ILigneCommande[] {
     let res: ILigneCommande[] = [];
-    this.cart.forEach((commande) => {
-      if(commande.produit?.quantiteDispo == 0) {
-        this.deleteCartProducts(commande)
+    this.cart.forEach(commande => {
+      if (commande.produit?.quantiteDispo == 0) {
+        this.deleteCartProducts(commande);
       }
       res = this.cart;
-    })
+    });
     return res;
   }
 }
